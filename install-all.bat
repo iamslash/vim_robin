771e1dfdf0f9c83d53ce5cbbb92c14216afb78eb
iamslash <iamslash@gmail.com>
2016-11-08 14:42:12 +0900

    success to install on windows

--- install-all.bat
+++ install-all.bat
@@ -1,20 +1,36 @@
 @ECHO OFF
 REM default variables
+SET VIM_HOME=D:\iamslash\.vim
 SET VIM_ROBIN_HOME=D:\iamslash\.vim_robin
-SET VIM_FILE=D:\iamslash\.vim
+SET VIM_FILE=D:\iamslash\.vimrc
 
 REM validate arguments
-IF EXIST %VIM_FILE% GOTO _ERROR_ALREADY_EXIST_VIM_FILE
+IF EXIST %VIM_HOME% GOTO _ERROR_ALREADY_EXIST_VIM_HOME
 IF EXIST %VIM_ROBIN_HOME% GOTO _ERROR_ALREADY_EXIST_VIM_ROBIN_HOME
+IF EXIST %VIM_FLIE% GOTO _ERROR_ALREADY_EXIST_VIM_FILE
 
 REM clone vim_robin
 git clone git@github.com:iamslash/vim_robin.git %VIM_ROBIN_HOME%
-mklink %VIM_FILE% %VIM_ROBIN_HOME%\.vim
+MKLINK %VIM_FILE% %VIM_ROBIN_HOME%\.vimrc
+MKDIR %VIM_HOME
+MKDIR %VIM_HOME\tags
+
+REM clone Vundle
+MKDIR %VIM_HOME\bundle
+git clone https://github.com/gmarik/Vundle.vim.git %VIM_HOME%\bundle\Vundle.vim  
+
+REM install
+vim +PluginInstall +qall
 
 ECHO Success to install vim_robin
 GOTO _END
 
 :_ERROR_ALREADY_EXIST_VIM_FILE
+ECHO ERROR)
+ECHO   Already exist .vimrc
+GOTO _END
+
+:_ERROR_ALREADY_EXIST_VIM_HOME
 ECHO ERROR)
 ECHO   Already exist .vim
 GOTO _END
@@ -25,4 +41,4 @@ ECHO   Already exist .vim_robin
 GOTO _END
 
 :_END
-REM ECHO %VIM_FILE%
+REM ECHO %VIM_HOME%
--- uninstall-all.bat
+++ uninstall-all.bat
@@ -1,10 +1,12 @@
 @ECHO OFF
 REM default variables
 SET VIM_ROBIN_HOME=D:\iamslash\.vim_robin
-SET VIM_FILE=D:\iamslash\.vim
+SET VIM_HOME=D:\iamslash\.vim
+SET VIM_FILE=D:\iamslash\.vimrc
 
 REM uninstall VIM_robin
-rmdir /s /q %VIM_ROBIN_HOME%
-del /S %VIM_FILE%
+rmdir /S /Q %VIM_ROBIN_HOME%
+rmdir /S /Q %VIM_HOME%
+del /F /S /Q %VIM_FILE%
 
 ECHO Success to uninstall vim_robin
